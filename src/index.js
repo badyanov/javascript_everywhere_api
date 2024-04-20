81c4d89aa50c5b76b5505cae2445d3d14861a506
Dmitriy <d.badyanov@outlook.com>
2024-04-21 01:03:45 +0400

    Hello world with GraphQL

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
 const express = require('express');
-const app = express();
 
 if (process.env.NODE_ENV !== 'dev') {
   console.log('This is prod configuration!');
   require('dotenv').config();
 }
 
+const { ApolloServer, gql } = require('apollo-server-express');
+
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => res.send('Hello, World from Express.js!'));
-app.listen(port, () =>
-  console.log(`Server running at http://localhost:${port}...`)
-);
+// Построение схемы с использованием GraphQL
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Функция разрешения для полей схемы
+const resolvers = {
+  Query: {
+    hello: () => 'Hello, world from QraphQL!',
+  },
+};
+
+const startApolloServer = async () => {
+  // Настройка ApolloServer
+  const server = new ApolloServer({ typeDefs, resolvers });
+
+  await server.start();
+  const app = express();
+
+  // Применяем промежуточное ПО Apollo GraphQL и указываем путь к API
+  server.applyMiddleware({ app, path: '/api' });
+
+  await new Promise((resolve) => app.listen({ port: port }, resolve));
+  console.log(
+    `🚀 GraphQL server running at http://localhost:${port}${server.graphqlPath}...`
+  );
+};
+
+startApolloServer(typeDefs, resolvers);
