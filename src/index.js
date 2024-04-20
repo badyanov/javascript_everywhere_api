537afe48c713c594e900616b7aa6c6c5311a0dc0
Dmitriy <d.badyanov@outlook.com>
2024-04-20 23:42:37 +0400

    hello world на express.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-// index.js
-// This is the main entry point of our application
+const express = require('express');
+const app = express();
+
+if (process.env.NODE_ENV !== 'dev') {
+  console.log('This is prod configuration!');
+  require('dotenv').config();
+}
+
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => res.send('Hello, World from Express.js!'));
+app.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}...`)
+);
