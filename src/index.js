681ec916e9d82b2837cdf17c20b1750fbf0e5ed8
badyanov <d.badyanov@outlook.com>
2024-04-29 22:49:08 +0400

    Базовый функционал api для приложения заметок с помощью GraphQL apollo server

--- src/index.js
+++ src/index.js
@@ -9,10 +9,33 @@ const { ApolloServer, gql } = require('apollo-server-express');
 
 const port = process.env.PORT || 4000;
 
+// Данные заметок временно хранятся в переменной notes
+let notes = [
+  { id: '1', author: 'Dmitriy', content: 'This is my first note!' },
+  { id: '2', author: 'Dmitriy', content: 'This is another my note!' },
+  {
+    id: '3',
+    author: 'Vasya',
+    content: "OMG! What's this? This is someone else's note!",
+  },
+];
+
 // Построение схемы с использованием GraphQL
 const typeDefs = gql`
   type Query {
-    hello: String
+    hello: String!
+    notes: [Note!]!
+    note(id: ID!): Note
+  }
+
+  type Note {
+    id: String
+    author: String
+    content: String
+  }
+
+  type Mutation {
+    newNote(content: String!): Note!
   }
 `;
 
@@ -20,15 +43,32 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     hello: () => 'Hello, world from QraphQL!',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find((note) => note.id === args.id);
+    },
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        author: 'Dmitriy',
+        content: args.content,
+      };
+      notes.push(noteValue);
+      return noteValue;
+    },
   },
 };
+
+const app = express();
+app.get('/', (req, res) => res.send('Hello, World from Express.js!'));
 
 const startApolloServer = async () => {
   // Настройка ApolloServer
   const server = new ApolloServer({ typeDefs, resolvers });
 
   await server.start();
-  const app = express();
 
   // Применяем промежуточное ПО Apollo GraphQL и указываем путь к API
   server.applyMiddleware({ app, path: '/api' });
